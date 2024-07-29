aa3763d930d7baa52161564e0ae7b531525563d8
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2024-07-29 13:08:44 +0530

    added routes and schema

--- server/index.js
+++ server/index.js
@@ -12,6 +12,13 @@ app.get('/', (req, res) => {
     res.send('Server is working')
 })
 
+//Importing Routes
+import userRoutes from './routes/user.js'
+
+//using routes
+app.use('/api', userRoutes);
+
+
 app.listen(port, () => { 
     console.log(`Server is running on http://localhost:${port}`);
     connectDb()
