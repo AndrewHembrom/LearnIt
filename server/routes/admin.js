738250cfe024fe4789e54cab02423d23378eeda5
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2024-07-29 19:53:31 +0530

    to add lectures

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { isAdmin, isAuth } from '../middlewares/isAuth.js';
-import { createCourse } from '../controllers/admin.js';
+import { addLEctures, createCourse } from '../controllers/admin.js';
 import { uploadFiles } from '../middlewares/multer.js';
 
 const router = express.Router();
 
 router.post('/course/new', isAuth, isAdmin, uploadFiles, createCourse);
+router.post('/course/:id', isAuth, isAdmin, uploadFiles, addLEctures);
 
 export default router;
