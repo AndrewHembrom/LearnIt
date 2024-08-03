72f15467041c1472cc372120ca71b714390346cc
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2024-08-03 21:09:20 +0530

    delete controllers for admin

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -2,6 +2,9 @@ import TryCatch from "../middlewares/TryCatch.js";
 import { Courses } from "../models/Courses.js";
 import { Lecture } from "../models/Lecture.js";
 import { rm } from 'fs';
+import { promisify } from "util";
+import fs from 'fs';
+import { User } from '../models/User.js';
 
 export const createCourse = TryCatch(async (req, res) => {
     const { title, description, category, createdBy, duration, price} = req.body;
@@ -58,5 +61,34 @@ export const deleteLecture = TryCatch(async (req, res) => {
 
     res.json({
         message: "Lecture Deleted"
+    })
+})
+
+const unlinkAsync = promisify(fs.unlink);
+
+export const deleteCourse = TryCatch(async (req, res) => {
+    const course = await Courses.findById(req.params._id);
+
+    const lectures = await Lecture.find({ course: course._id })
+    
+    await Promise.all(
+        lectures.map(async (lecture) => {
+            await unlinkAsync(lecture.video);
+            console.log("Video Deleted");
+        })
+    )
+
+    rm(course.image, () => { 
+        console.log("Image Deleted");
+    })
+
+    await Lecture.find({ course: req.params._id }).deleteMany()
+    
+    await course.deleteOne();
+
+    await User.updateMany({}, { $pull: { subscription: req.params._id } });
+
+    res.json({
+        message: "Course Deleted"
     })
 })
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { isAdmin, isAuth } from '../middlewares/isAuth.js';
-import { addLEctures, createCourse, deleteLecture } from '../controllers/admin.js';
+import { addLEctures, createCourse, deleteCourse, deleteLecture } from '../controllers/admin.js';
 import { uploadFiles } from '../middlewares/multer.js';
 
 const router = express.Router();
 
 router.post('/course/new', isAuth, isAdmin, uploadFiles, createCourse);
 router.post('/course/:id', isAuth, isAdmin, uploadFiles, addLEctures);
-router.delete('/lecture/:id', isAuth, isAdmin, deleteLecture)
+router.delete('/lecture/:id', isAuth, isAdmin, deleteLecture);
+router.post('/course/:id', isAuth, isAdmin, deleteCourse);
 
 export default router;
