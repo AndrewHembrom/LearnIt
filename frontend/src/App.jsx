dc8984c98ac152a3172bcfe264ab95af1ec20b91
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2024-08-04 18:23:20 +0530

    Added Auth pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home.jsx'
 import Header from './components/Header/Header.jsx';
+import Login from './pages/auth/Login.jsx';
+import Register from './pages/auth/Register.jsx';
+import Verify from './pages/auth/Verify.jsx';
 
 const App = () => {
   return (
@@ -10,7 +13,10 @@ const App = () => {
       <BrowserRouter>
         <Header/>
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/verify' element={<Verify/>}/>
         </Routes>
       </BrowserRouter>
     </div>
